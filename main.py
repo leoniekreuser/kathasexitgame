c6b08fcb64e89181bddb92994be5772ce299059d
Leonie Kreuser <leonie.kreuser@alexanderthamm.com>
2025-11-23 19:55:25 +0100

    test-commit cicd

--- main.py
+++ main.py
@@ -11,21 +11,20 @@ selection_user = st.selectbox("Choose an option:", ["Schere ✂️", "Stein 🪨
 selection_computer = random.choice(["Schere ✂️", "Stein 🪨", "Papier 📄"])
 
 st.write(f"You selected: {selection_user}")
-#st.write(f"Computer selected: {selection_computer}")
-#who wins? 
+# st.write(f"Computer selected: {selection_computer}")
+# who wins?
 time.sleep(3)
 if selection_user == selection_computer:
     st.write("It's a tie!")
-elif (selection_user == "Schere ✂️" and selection_computer == "Papier 📄") or \
-     (selection_user == "Stein 🪨" and selection_computer == "Schere ✂️") or \
-     (selection_user == "Papier 📄" and selection_computer == "Stein 🪨"):
+elif (
+    (selection_user == "Schere ✂️" and selection_computer == "Papier 📄")
+    or (selection_user == "Stein 🪨" and selection_computer == "Schere ✂️")
+    or (selection_user == "Papier 📄" and selection_computer == "Stein 🪨")
+):
     st.write("You win!")
 else:
     st.write("Computer wins!")
 
 
 st.write("Was für ein tolles Spiel!")
-
-
-print("ooo")
-print("bbbb")
+st.write("Wenn das hier sichtbar ist, hat die CI/CD Pipeline funktioniert.")
